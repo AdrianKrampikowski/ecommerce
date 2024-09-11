72176f0031ce8ab987068fe236bac2676884fc60
AdrianKrampikowski <info@adrian-krampikowski.com>
2024-09-11 16:27:09 +0200

    frontennd style

--- storefront/src/app/app.routes.ts
+++ storefront/src/app/app.routes.ts
@@ -1,3 +1,5 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: HomepageComponent }];
