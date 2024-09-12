f7fd30354a6f8b9c3bccaa82b94c46a7c5469450
AdrianKrampikowski <info@adrian-krampikowski.com>
2024-09-12 18:03:10 +0200

    style prodcuts page

--- admin_dashboard/src/app/components/product/product-list/product-list.component.ts
+++ admin_dashboard/src/app/components/product/product-list/product-list.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatTableModule],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.scss'
+  styleUrl: './product-list.component.scss',
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  ELEMENT_DATA: any[] = [
+    {
+      quader: 'quader',
+      product: {
+        img: '../../../../assets/img/testImg.png',
+        name: 'product',
+      },
+      status: 'status',
+      sales_channel: 'sales_channel',
+      markets: 'markets',
+      category: 'category',
+      type: 'type',
+      vendor: 'vendor',
+    },
+  ];
+  displayedColumns: string[] = [
+    'quader',
+    'product',
+    'status',
+    'sales_channel',
+    'markets',
+    'category',
+    'type',
+    'vendor',
+  ];
+  dataSource: any;
 
+  ngOnInit(): void {
+    this.dataSource = this.ELEMENT_DATA;
+  }
 }
