d8ff423e7e4c635dc2032b535ca924eef0c13924
AdrianKrampikowski <info@adrian-krampikowski.com>
2024-09-13 15:32:03 +0200

    restyle sidebar

--- admin_dashboard/src/app/shared/sidebar/sidebar.component.ts
+++ admin_dashboard/src/app/shared/sidebar/sidebar.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { DatePipe } from '@angular/common';
 import { MatListModule } from '@angular/material/list';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [
+    CommonModule,
     MatSidenavModule,
     MatListModule,
     MatIconModule,
     MatDividerModule,
     DatePipe,
+    MatExpansionModule,
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
@@ -25,23 +29,24 @@ export class SidebarComponent {
       name: 'Home',
     },
     {
-      img: 'home',
+      img: 'local_shipping',
       name: 'Orders',
     },
     {
-      img: 'home',
-      name: 'Prodcuts',
+      img: 'store',
+      name: 'Products',
+      subItems: [
+        { name: 'Electronics' },
+        { name: 'Apparel' },
+        { name: 'Books' },
+      ],
     },
     {
-      img: 'home',
-      name: 'Sub Collection',
-    },
-    {
-      img: 'home',
+      img: 'people',
       name: 'Customers',
     },
     {
-      img: 'home',
+      img: 'local_offer',
       name: 'Discounts',
     },
   ];
