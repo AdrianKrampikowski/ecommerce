daef73f87c42e924157d7f915d654860753e51b2
AdrianKrampikowski <info@adrian-krampikowski.com>
2024-09-14 09:09:31 +0200

    restyle sidebar

--- ecom_backend/apis/users/index.js
+++ ecom_backend/apis/users/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const userController = require("./controller");
 const auth = require("../../middleware/auth");
 
+
 router.route("/signup").post(userController.signUp);
 router.route("/login").post(userController.login);
 router.route("/getAllUsers").get(auth, userController.getAllUsers);
 router.route("/getUser").post(userController.getUser);
+
 
 module.exports = router;
