6f25ef86fe01d46c0b9fa69d25b2842346a6b01e
AdrianKrampikowski <info@adrian-krampikowski.com>
2024-09-16 16:11:16 +0200

    restyle homepage

--- storefront/src/app/components/homepage-collection-list/homepage-collection-list.component.ts
+++ storefront/src/app/components/homepage-collection-list/homepage-collection-list.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-homepage-collection-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './homepage-collection-list.component.html',
-  styleUrl: './homepage-collection-list.component.scss'
+  styleUrl: './homepage-collection-list.component.scss',
 })
 export class HomepageCollectionListComponent {
-
+  images: string[] = [
+    '../../../assets/img/Image20240911163322.png',
+    '../../../assets/img/Image20240911164557.png',
+  ];
 }
